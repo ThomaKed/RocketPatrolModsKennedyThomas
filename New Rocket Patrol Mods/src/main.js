646b0489eafc6bc86f434c26b83baa5e6738a325
Doughboi-Games <78245556+Doughboi-Games@users.noreply.github.com>
2022-04-19 00:19:57 -0700

    Fifth Commit - UI and Sound Changed

--- New Rocket Patrol Mods/src/main.js
+++ New Rocket Patrol Mods/src/main.js
@@ -13,9 +13,19 @@ let keyF, keyR, keyLEFT, keyRIGHT;
 let borderUISize = game.config.height / 15;
 let borderpadding = borderUISize / 3;
 
-//Bandit Buster by Kennedy Thomas 04/18/2022. The game took me 'X' hours to complete.
-//Point Break down - 105 Max points
+//Bandit Buster by Kennedy Thomas 04/18/2022. The game took me '15' hours to complete.
+
+//Credits
+//Shooting Gun Sound Effect - 9mm pistol shoot short reverb by gattoangus
+//Select sound - 1911 Reload by Nioczkus
+//Explosion Sound - Explosion_03 by Little Robot Sound Factory
+//BGMusic - Wild West Story by Soundroll
+//Emery on the discord for helping me with the timer function.
+
+
+//Point Break down - 100 Max points
 //Allow the player to control the Rocket after it's fired (5)
 //Display the time remaining (in seconds) on the screen (10)
 //Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
 //Add your own (copyright-free) background music to the Play scene (5)
+//Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
